8592ef4c587a56b7224bcf86411a05409463f4bf
SP4CEVI <95271930+SP4CEVI@users.noreply.github.com>
2024-03-17 11:39:22 +0300

    Update taylor.fsx

--- lab1/taylor.fsx
+++ lab1/taylor.fsx
@@ -1,41 +1,36 @@
-let eps:float = 0.0000001
+let eps: float = 0.00000001
 
-let f x:float =  (1. + x ** 2.) / 2. * System.Math.Atan(x) - x / 2.
-let f_iter (x:float) (n:int) = (-1.) ** n * (-1.) * (x ** n * x ** n * x) / (4. * float(n) * float(n) - 1.)
+let f x: float = (1. + x ** 2.) / 2. * System.Math.Atan(x) - x / 2.
 
-let a:float = 0.1
-let b:float = 0.6
-let n:int = 10
+let rec f_it (x: float) (n: int) =
+    if n = 0 then
+        1.
+    else
+        let f_it_minus_1 = f_it x (n - 1)
+        f_it_minus_1 * (x ** (2. * (float n) + 1.)) / (4. * float(n) * float(n) - 1.)
 
-let taylor_naive x:float = 
-    let mutable i:int = 1
-    let mutable sum:float = 0.
-    while abs (f_iter x i) > eps do
-        sum <- sum + f_iter x i
-        i <- i + 1
-    sum    
-        
-let taylor x:float =
-    let mutable sum:float = 0.
-    let mutable befor_s:float = 1.
-    let mutable befor_sd:float = 5.
-    let mutable befor:float = 1. / 3.
-    let mutable befor_x:float = x ** 3.
-    let mutable f_iter:float = befor * befor_x
-    while abs (f_iter) > eps do
-        sum <- sum + f_iter
-        befor_s <- befor_s + 2.
-        befor_sd <- befor_sd + 2.
-        befor <- befor * befor_s / befor_sd
-        befor_x <- befor_x * x * x
-        f_iter <- befor_x * befor
-    sum
+let a: float = 0.1
+let b: float = 0.6
+let n: int = 10
 
+let rec taylor_naive x i sum =
+    if i <= n then
+        let term = f_it x i
+        taylor_naive x (i + 1) (sum + term)
+    else
+        sum
 
+let rec taylor x sum befor_s befor_sd befor befor_x =
+    let f_it = befor_x * befor
+    if abs f_it > eps then
+        taylor x (sum + f_it) (befor_s + 2.) (befor_sd + 2.) (befor * (befor_s / befor_sd)) (befor_x * x * x)
+    else
+        sum
 
-let main:unit =
-    for i=0 to n do
-        let x:float = a + (float i) / (float n) * (b - a)
-        printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
+let main =
+    for i = 0 to n do
+        let x: float = a + float i / float n * (b - a)
+        let taylor_result = taylor_naive x 0 0.
+        printfn "%5.2f  %10.5f  %10.5f   %10.5f" x (f x) (f_it x 1) (taylor x 0. 1. 5. (1./3.) (x ** 3.))
 
 main
